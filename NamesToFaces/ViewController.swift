04b25f8d8af36add0ace5d5a9d31f1fec1328048
aajuvale <ahmed.juvale@gmail.com>
2025-08-24 06:38:28 -0700

    feat: implemented renaming and deleting

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -78,18 +78,33 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
 
-        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
-        ac.addTextField()
+        // Challenge 1: Rename or Delete
+        let pickerAc = UIAlertController(
+            title: "Do you want to rename a person or delete a person?",
+            message: nil,
+            preferredStyle: .alert
+        )
+        pickerAc.addAction(UIAlertAction(title: "Rename", style: .default, handler: { _ in
+            let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+            ac.addTextField()
 
-        ac.addAction(UIAlertAction(title: "OK", style: .default) {
-            [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
-            self?.collectionView.reloadData()
-        })
+            ac.addAction(UIAlertAction(title: "OK", style: .default) {
+                [weak self, weak ac] _ in
+                guard let newName = ac?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+            })
 
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-        present(ac, animated: true)
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            self.present(ac, animated: true)
+        }))
+
+        pickerAc.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
+            self.people.remove(at: indexPath.item)
+            self.collectionView.reloadData()
+        }))
+
+        present(pickerAc, animated: true)
     }
 }
 
