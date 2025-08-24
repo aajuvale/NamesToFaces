6fffc8408cd1f1a7c6c8347d1d2a3c0a18a623f1
aajuvale <ahmed.juvale@gmail.com>
2025-08-24 06:14:02 -0700

    feat: added NSObject class

---
+++ NamesToFaces/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Ahmed Juvale on 8/24/25.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +50,10 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
             try? jpegData.write(to: imagePath)
         }
 
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
         dismiss(animated: true)
     }
 
