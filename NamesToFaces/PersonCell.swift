1fad21a8f94d6639fdd1e73071a33a61868af408
aajuvale <ahmed.juvale@gmail.com>
2025-08-23 20:25:30 -0700

    feat: added connections

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Ahmed Juvale on 8/23/25.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+
+    
+}
